8cba6b548a7c270e1ef94b9974ef986eaa937392
Enrique <ebhenriquez89@gmail.com>
2020-05-14 18:19:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Real-time Hand-Detection using Neural Networks (SSD) on Tensorflow + Gloves detection
+
+
+![Otro meme más](images\gloves.jpg)
+
+Este repositorio tiene como misión detectar si las manos de un usuario que se ponen delante de la webcam tienen guantes o no. 
+Para ello se hace uso de un repositorio cuyo enlace se puede ver a continuación y de un modelo de entrenador en **Teachable Machine** (disponible en la carpeta `gloves-checkpoint`).
+
+Para entrenar el modelo decisión machine se han tomado aproximadamente unas 1000 fotografías de manos con guantes (`gloves-on`) y otras 1000 fotografías de manos sin guantes (`gloves-off`) en diversas posiciones. Para poder servir de manera rápida este modelo lanzamos una predicción sobre cada imagen servirda por la webcam y lo mezclamos con el modelo de detección y seguimiento de manos.
+
+Recomendamos realizar o crear un entorno virtual de acuerdo al fichero `environment.yml` incluido en el repositorio.
+
+Para poder hacer uso texto prueba de concepto simplemente ejecutar el script.
+```bash
+$ python detect_single_threaded_gloves.py
+```
+Un ejemplo rápido de cómo funciona se facilita en el [siguiente vídeo](gloves-detector-PoC.mp4).
+
+En la carpeta [Teachable Machine Gloves Detection](Teachable-Machine-Gloves-Detection) se incluye todo el material descargable de ese servicio.
---
+++ detect_single_threaded_gloves.py
@@ -0,0 +1,158 @@
+from utils import detector_utils as detector_utils
+import cv2
+import tensorflow as tf
+import datetime
+import argparse
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model for gloves deection
+model = tensorflow.keras.models.load_model('gloves-checkpoint/keras_model.h5')
+# Las etiquetas con las que se ha entrenado este modelo son:
+labels = ["gloves-off", "gloves-on"]
+
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1.
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+
+detection_graph, sess = detector_utils.load_inference_graph()
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-sth',
+        '--scorethreshold',
+        dest='score_thresh',
+        type=float,
+        default=0.2,
+        help='Score threshold for displaying bounding boxes')
+    parser.add_argument(
+        '-fps',
+        '--fps',
+        dest='fps',
+        type=int,
+        default=1,
+        help='Show FPS on detection/display visualization')
+    parser.add_argument(
+        '-src',
+        '--source',
+        dest='video_source',
+        default=0,
+        help='Device index of the camera.')
+    parser.add_argument(
+        '-wd',
+        '--width',
+        dest='width',
+        type=int,
+        default=640,
+        help='Width of the frames in the video stream.')
+    parser.add_argument(
+        '-ht',
+        '--height',
+        dest='height',
+        type=int,
+        default=480,
+        help='Height of the frames in the video stream.')
+    parser.add_argument(
+        '-ds',
+        '--display',
+        dest='display',
+        type=int,
+        default=1,
+        help='Display the detected images using OpenCV. This reduces FPS')
+    parser.add_argument(
+        '-num-w',
+        '--num-workers',
+        dest='num_workers',
+        type=int,
+        default=8,
+        help='Number of workers.')
+    parser.add_argument(
+        '-q-size',
+        '--queue-size',
+        dest='queue_size',
+        type=int,
+        default=5,
+        help='Size of the queue.')
+    args = parser.parse_args()
+
+    cap = cv2.VideoCapture(args.video_source)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, args.width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, args.height)
+
+    start_time = datetime.datetime.now()
+    num_frames = 0
+    im_width, im_height = (cap.get(3), cap.get(4))
+    # max number of hands we want to detect/track
+    num_hands_detect = 2
+
+    cv2.namedWindow('Single-Threaded Detection', cv2.WINDOW_NORMAL)
+
+    while True:
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        ret, image_np = cap.read()
+        # image_np = cv2.flip(image_np, 1)
+        try:
+            image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+        except:
+            print("Error converting to RGB")
+        
+        #resize the image to a 224x224 with the same strategy as in TM2:
+        #resizing the image to be at least 224x224 and then cropping from the center
+        size = (224, 224)
+        # image = ImageOps.fit(image_np, size, Image.ANTIALIAS)
+        image = cv2.resize(image_np, size)
+
+        #turn the image into a numpy array
+        image_array = np.asarray(image)
+
+        # Normalize the image
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+        # Load the image into the array
+        data[0] = normalized_image_array
+
+        # run the inference
+        prediction = model.predict(data)
+
+        # Actual detection. Variable boxes contains the bounding box cordinates for hands detected,
+        # while scores contains the confidence for each of these boxes.
+        # Hint: If len(boxes) > 1 , you may assume you have found atleast one hand (within your score threshold)
+
+        boxes, scores = detector_utils.detect_objects(image_np,
+                                                      detection_graph, sess)
+
+        # draw bounding boxes on frame
+        detector_utils.draw_box_on_image(num_hands_detect, args.score_thresh,
+                                         scores, boxes, im_width, im_height,
+                                         image_np)
+
+        # Calculate Frames per second (FPS)
+        num_frames += 1
+        elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
+        fps = num_frames / elapsed_time
+
+        if (args.display > 0):
+            # Display FPS on frame
+            if (args.fps > 0):
+                # detector_utils.draw_fps_on_image("fps : " + str(int(fps)),
+                #                                  image_np)
+                detector_utils.draw_label_on_image(labels[np.argmax(prediction[0])],
+                                                 image_np)
+
+            cv2.imshow('Single-Threaded Detection',
+                       cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+        else:
+            print("frames processed: ", num_frames, "elapsed time: ",
+                  elapsed_time, "fps: ", str(int(fps)))
